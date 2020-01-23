842c7146d383f4063ef567d6152030e326fc820a
williamstravis <travis.williams@nrel.gov>
2020-01-23 11:33:36 -0700

    just a few changes

--- functions.py
+++ functions.py
@@ -8,17 +8,24 @@ Created on Wed Jan  8 09:05:39 2020
 @author: twillia2
 """
 import os
-import geopandas as gpd
-import numpy as np
-import pandas as pd
 import shutil
-from glob import glob
+import geopandas as gpd
+import h5py
+import numpy as np
+from fiona.errors import DriverError
+from geofeather import from_geofeather
 from osgeo import gdal, ogr, osr
 from shapely.geometry import Point
 
+# VARIABLES
+ALBERS_PROJ4 = ("+proj=aea +lat_0=40 +lon_0=-96 +lat_1=20 +lat_2=60 +x_0=0 " +
+                "+y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
+
+# Wind Toolkit Point Coordinates
+WTK = from_geofeather("data/shapefiles/wtk_points.feather")
 
 # FUNCTIONS
-def to_geo(df, loncol="lon", latcol="lat", epsg=4326):
+def to_geo(data_frame, loncol="lon", latcol="lat", epsg=4326):
     """ Convert a Pandas DataFrame object to a GeoPandas GeoDataFrame object.
 
     Parameters
@@ -39,13 +46,13 @@ def to_geo(df, loncol="lon", latcol="lat", epsg=4326):
     """
     crs = {"init": "epsg:{}".format(epsg)}
     to_point = lambda x: Point((x[loncol], x[latcol]))
-    df["geometry"] = df.apply(to_point, axis=1)
-    gdf = gpd.GeoDataFrame(df, geometry="geometry", crs=crs)
+    data_frame["geometry"] = data_frame.apply(to_point, axis=1)
+    gdf = gpd.GeoDataFrame(data_frame, geometry="geometry", crs=crs)
 
     return gdf
 
 
-def rasterize(src, dst, attribute, resolution, epsg, extent, na=-9999,
+def rasterize(src, dst, attribute, resolution, epsg, extent, navalue=-9999,
               all_touch=False, overwrite=False):
     """Use GDAL Rasterize to rasterize a shapefile stored on disk and write
     outputs to a file.
@@ -97,7 +104,7 @@ def rasterize(src, dst, attribute, resolution, epsg, extent, na=-9999,
     cols = int((xmax - xmin)/resolution)
     rows = int((ymax - ymin)/resolution) + 1
     trgt = gdal.GetDriverByName("GTiff").Create(dst, cols, rows, 1,
-                                gdal.GDT_Float32)
+                                                gdal.GDT_Float32)
     trgt.SetGeoTransform((xmin, resolution, 0, ymax, 0, -resolution))
 
     # Add crs
@@ -107,7 +114,7 @@ def rasterize(src, dst, attribute, resolution, epsg, extent, na=-9999,
 
     # Set no value
     band = trgt.GetRasterBand(1)
-    band.SetNoDataValue(na)
+    band.SetNoDataValue(navalue)
 
     # Set options
     if all_touch is True:
@@ -155,7 +162,7 @@ def read_raster(rasterpath, band=1, navalue=-9999):
     array = array.astype(float)
     if np.nanmin(array) < navalue:
         navalue = np.nanmin(array)
-    array[array==navalue] = np.nan
+    array[array == navalue] = np.nan
 
     return(array, geometry, arrayref)
 
@@ -241,7 +248,7 @@ def reproject_polygon(src, dst, tproj):
         src_feature = src_layer.GetNextFeature()
 
     # Close both shapefiles
-    src_file= None
+    src_file = None
     trgt_file = None
 
 
@@ -327,8 +334,78 @@ def reproject_point(src, dst, tproj):
         src_feature = src_layer.GetNextFeature()
 
     # Close both shapefiles
-    src_file= None
+    src_file = None
     trgt_file = None
+
+
+def shp_to_h5(shp, hdf, attribute, mode="w"):
+    """Assign values from a shapefile of polygons to the WTK point dataset
+    return a geopandas geodataframe and write or append to an HDF5 file.
+
+    Parameters
+    ----------
+    shp : geopandas.geodataframe.GeoDataFrame | str
+        Either a GeoPandas GeoDataFrame or a path as a string to a shapefile
+    attribute : str
+        The attribute of the shapefile from which to assign values to the WTK
+        point data set.
+
+    Returns
+    -------
+    geopandas.geodataframe.GeoDataFrame
+
+    Mode Notes
+    ----------
+        r         : 	Readonly, file must exist
+        r+        : 	Read/write, file must exist
+        w         : 	Create file, truncate if exists
+        w- or x   : 	Create file, fail if exists
+        a         : 	Read/write if exists, create otherwise (default)
+
+    I want this to either create a new HDF5 file of cost layer values at each
+    wind toolkit point, or append to an existing hdf dataset with wtk points.
+    Grant will have the best idea of what is needed to incorporate our changes
+    into reV.
+
+    I should do multiple attributes at once.
+
+    Also, this drops non-present values...
+
+    It's a start
+    """
+
+    # Is this file in memory or on disk?
+    if isinstance(shp, str):
+        try:
+            shp = gpd.read_file(shp)
+        except DriverError:
+            print("Shapefile doesn't exist.")
+
+    # Extract just the requested attribute
+    shp = shp[["geometry", attribute]]
+    wtk2 = gpd.sjoin(WTK, shp)
+
+    # HDF5 doesn't like type 'O'
+    dtype = wtk2[attribute].dtype
+
+    if mode == "w":
+        with h5py.File(hdf, mode) as file:
+            if dtype is np.dtype('O'):
+                file.create_dataset(name=attribute, data=wtk2[attribute],
+                                    dtype=h5py.string_dtype(encoding='utf-8'))
+            else:
+                file.create_dataset(name=attribute, data=wtk2[attribute])
+            file.create_dataset(name="lat", data=wtk2["lat"])
+            file.create_dataset(name="lon", data=wtk2["lon"])
+    elif mode == "a":
+        with h5py.File(hdf, mode) as file:
+            if dtype is np.dtype('O'):
+                file.create_dataset(name=attribute, data=wtk2[attribute],
+                                    dtype=h5py.string_dtype(encoding='utf-8'))
+            else:
+                file.create_dataset(name=attribute, data=wtk2[attribute])
+    else:
+        print("Sorry, I haven't figured mode=" + mode + " out yet.")
 
 
 def to_raster(array, savepath, crs, geometry, navalue=-9999):
@@ -350,8 +427,6 @@ def to_raster(array, savepath, crs, geometry, navalue=-9999):
         The number used for non-values in the raster data set.
     """
     # Retrieve needed raster elements
-    geometry = geometry
-    crs = crs
     xpixels = array.shape[1]
     ypixels = array.shape[0]
 
--- scratch.py
+++ scratch.py
@@ -24,7 +24,7 @@ from geofeather import from_geofeather, to_geofeather
 import geopandas as gpd
 import pandas as pd
 from glob import glob
-from functions import to_geo, rasterize, reproject_polygon, reproject_point
+from functions import shp_to_h5
 
 # I might share this on github
 os.chdir(os.path.expanduser("~/github/weto"))
@@ -34,119 +34,14 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
-
-# 1) Create Grids of WTK GIDS
-# Point file saved as geofeather for faster I/O
-if not os.path.exists("data/shapefiles/wtk_points.feather"):
-    wtkpnts = pd.read_csv("data/tables/wind_conus_v1_coords.csv")
-    wtkpnts["gid"] = wtkpnts.index
-    wtk = to_geo(wtkpnts, loncol="lon", latcol="lat", epsg=4326)
-    to_geofeather(wtk, "data/shapefiles/wtk_points.feather")
-else:
-    wtk = from_geofeather("data/shapefiles/wtk_points.feather")
-
-# Write to shapefile or geopackage
-if not os.path.exists("data/shapefiles/wtk_points.shp"):
-    wtk.to_file("data/shapefiles/wtk_points.shp")
-
-# Reproject to NA Alber Equal Area Conic <------------------------------------- What do we usually use?
-tproj = ("+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 " +
-         "+y_0=0 +datum=NAD83 +units=m +no_defs")
-if not os.path.exists("data/shapefiles/wtk_points_albers.shp"):
-    reproject_point(src="data/shapefiles/wtk_points.shp",
-                    dst="data/shapefiles/wtk_points_albers.shp",
-                    tproj=tproj)
-wtk_albers = gpd.read_file("data/shapefiles/wtk_points_albers.shp")
-
-# Rasterize both geographic and projected WTK points
-src = "data/shapefiles/wtk_points.shp"
-dst = "data/rasters/test.tif"
-xmin, ymin, xmax, ymax = wtk.geometry.total_bounds
-rasterize(src=src, dst=dst, attribute="gid", resolution=0.02, epsg=4326,
-          extent=[xmin, ymin, xmax, ymax], overwrite=False)
-
-src = "data/shapefiles/wtk_points_albers.shp"
-dst = "data/rasters/test_albers.tif"
-xmin, ymin, xmax, ymax = wtk_albers.geometry.total_bounds
-rasterize(src=src, dst=dst, attribute="gid", resolution=, epsg=102008,
-          extent=[xmin, ymin, xmax, ymax], overwrite=True)
-
-
-
-
 # A sample shapefile layer
-datapath = os.path.expanduser("~/Box/WETO 1.2/data")
-samplepath = glob(os.path.join(datapath, "BLM", "fedlands_blm.shp"))[0]
-sample = gpd.read_file(samplepath)
+shp = "/Users/twillia2/Box/WETO 1.2/data/BLM/fedlands_blm.shp"
+trgt = "data/tables/wtk_fedland.h5"
 
-
-# 1) Create a GeoTiff from a shapefile.
-shp =  "/Users/twillia2/Box/WETO 1.2/data/BLM/fedlands_blm.shp"
-trgt = "data/fedlands.tif"
-
-#
-
-
-
-# 1) Create an HDF5 data set that lines up with the WTK CONUS data set
+# Shapefile to HDF5 data set
+gdf = gpd.read_file(shp)
+shp_to_h5(gdf, trgt, attribute="raster_val", mode="a")
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Let's clip by extent
-xmin, ymin, xmax, ymax = wtk.geometry.total_bounds
-test = sample.cx[xmin:xmax, ymin:ymax]
-
-# We'll probably want to store these as both a geotiff and an hdf5 file
-# GeoTiff
-
-
-
-
-# Save to shapefile
-test.to_file("data/test.shp")
-
-# I can't infer the proper resolution, the projection created slightly
-# different spacing...need to discuss this
-src = "data/test.shp"
-dst = "data/test_albers2.shp"
-
-reproject_polygon(src, dst, tproj)
-
-
-# WTK Points
-wtk.to_file("data/wtk.shp")
-src = "data/wtk.shp"
-dst = "data/wtk_albers.shp"
-reproject_point(src, dst, tproj)
-
-
-
-
-
-# Is the project resolution consistent?
-albers = gpd.read_file("data/test_albers2.shp")
-
-src = "data/test_albers2.shp"
-dst = "data/test_albers.tif"
-rasterize(src=src, dst=dst, attribute="raster_val", resolution=0.02, epsg=4326,
-          extent=[xmin, ymin, xmax, ymax], overwrite=False)
-
-
-test.plot(column="state")
