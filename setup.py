51e9a7d6a7ab843b63be661e284d30ea899df94a
williamstravis <travis.williams@nrel.gov>
2020-01-30 11:27:00 -0700

    packaged weto up, almost got the end of nlcd step

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Install methods for calculating cost surfaces for the LandBOSSE wind production
+model. 
+
+Created on Thu Jan 30 08:50:16 2020
+
+@author: twillia2
+"""
+
+from setuptools import setup
+
+setup(
+    name="weto",
+    version="0.0.1",
+    packages=["weto"],
+    description=("Methods for calculating cost surfaces for the LandBOSSE " +
+                 "wind production model"),
+    author="Travis Williams",
+    author_email="travis.williams@nrel.gov",
+    install_requires=["dask", "dask-jobqueue", "descartes", "distributed",
+                      "geopandas", "h5py", "scipy", "tqdm"]
+    )
---
+++ weto/__init__.py
@@ -0,0 +1,3 @@
+"""
+weto initialization
+"""
