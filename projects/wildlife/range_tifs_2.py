664226c5573915d49fb97f29799e299283d3069c
williamstravis <travis.williams@nrel.gov>
2020-03-27 11:29:48 -0600

    Adding a new method for categorizing overlapping wildlife ranges

--- projects/wildlife/range_tifs_2.py
+++ projects/wildlife/range_tifs_2.py
@@ -2,11 +2,14 @@
 Build overlapping wildlife range tiffs
 """
 
-import dask.array as da
-from itertools import combinations
-import numpy as np
 import os
+
+from itertools import combinations
+
+import dask.array as da
+import numpy as np
 import rasterio
+from tqdm import tqdm
 import xarray as xr
 
 from numpy.random import randint
@@ -44,20 +47,22 @@ RANGE_CATEGORIES = {
 CHUNKS = {"band": 1, "x": 5000, "y": 5000}
 
 
-def seq_int(seq):
-    """Concatenate a list of integers.
-    seq = (5, 1, 4, 3)
-    """
-    if len(seq) > 0:
-        if len(seq) > 1:
-            frame = "".join(["{}" for i in range(len(seq))])
-            return int(frame.format(*seq))
-        else:
-            return int("{}".format(seq[0])) 
 
 
-def array_int(array):
-    return np.apply_along_axis(seq_int, 0, array)
+def combo_check(path_dict):
+    """Check how many possible combinations of a set of ranges can be made.
+    We can use this to check against the number of unique products we use
+    as category values."""
+
+    keys = path_dict.keys()
+    
+    combos = [[c for c in combinations(keys, i + 1)] for i in range(len(keys))]
+    combos = [c for sc in combos for c in sc]
+    combos = [sorted(c) for c in combos]
+    combos = np.unique(combos)
+    ncombos = len(combos)
+
+    return ncombos
 
 
 def code_range(path_dict):
@@ -65,52 +70,82 @@ def code_range(path_dict):
     path_dict = RANGE_CATEGORIES[5]
     """
     
-    # Create a numeric dictionary for these keys
-    key_dict = {key: i + 1  for i, key in enumerate(path_dict.keys())}
+    # Create a dictionary of random values 
+    key_dict = {}
+    for i, key in enumerate(path_dict.keys()):
+        val = np.random.randint(1, 100)
+        key_dict[key] = val
+    try:
+        assert len(path_dict) == len(key_dict)
+    except AssertionError:
+        msg = "\n\nNot all random keys are unique, try again."
+        raise AssertionError(msg) 
+
+    # We'll need to associate the numbers with categories
     number_dict = {k: i for i, k in key_dict.items()}
     vals = key_dict.values()
+ 
+    # Find all unique products
     combos = [[c for c in combinations(vals, i + 1)] for i in range(len(vals))]
     combos = [c for sc in combos for c in sc]
-    combo_keys = {}
-    for combo in combos:
-        key = "-".join([number_dict[c] for c in combo])
-        value = seq_int(combo)
-        combo_keys[key] = value
+    products = np.unique([np.prod(c) for c in combos])
+
+    nproduct = len(products)
+    check = combo_check(path_dict)
+    try:
+        assert nproduct == check
+    except AssertionError:
+        msg = "\n\nNot all products are unique, try something else."
+        raise AssertionError(msg) 
+    
+
+    # combo_keys = {}
+    # for combo in combos:
+    #     key = "-".join([number_dict[c] for c in combo])
+    #     value = seq_int(combo)
+    #     combo_keys[key] = value
 
     # Assign each raster a unique value
+    print("Stacking rasters and assigning unique values...")
     arrays = []
-    for key, path in path_dict.items():
+    for key, path in tqdm(path_dict.items(), position=0):
         value = key_dict[key]
         full_path = DP.join(path)
-        array = xr.open_rasterio(full_path, chunks=CHUNKS)[0].data
+        ds = xr.open_rasterio(full_path)
+        navalue = ds.attrs["nodatavals"][0]
+        array = ds[0].data
         array[da.isnan(array)] = 0
+        if not np.isnan(navalue):
+            array[array == navalue] = 0
         array[array > 0] = value
+        array[array == 0] = 1
         arrays.append(array)
 
-    # Stack everything together - we might have to save this a temporary file
-    stack = da.stack(arrays, axis=0)
-    stack = stack.rechunk((stack.shape[0], 5000, 5000))
-    stack = stack[:, 4000: 10000, 4000: 10000]
+    # Take the product, these will be in the code keys above
+    stack = np.stack(arrays, axis=0)
+    layer = np.prod(stack, axis=0)
 
-    # Try to map the function to each point
-    client = Client()
-    codes = da.apply_along_axis(seq_int, 0, stack, dtype="uint8")
-    future = client.compute(codes)
-    result = future.result()
-    client.shutdown()
-    client.close()
+    # These numbers are no good, we need to reset them
+    uvals  = np.unique(layer)
+    reset_dict = {val: i + 1  for i, val in enumerate(uvals)}
+    for old, new in tqdm(reset_dict.items(), position=0):
+        layer[layer == old] = new
+
+    # These will now be small enough for this
+    layer = layer.astype("uint8")
 
     # Save to temp and delete
     template = rasterio.open(full_path)
     temp_path = DP2.join("test.tif")
     with rasterio.Env():
         profile = template.profile
+        profile["dtype"] = layer.dtype.name
         profile.update(
             dtype=rasterio.uint8,
             count=1,
             compress='lzw')
         with rasterio.open(temp_path, 'w', **profile) as dst:
-            dst.write(result)
+            dst.write(layer, 1)
 
 
 if __name__ == "__main__":
